8fffc90b371da042ef4c363783eb43cf445327ff
nageshkumarbs007@gmail.com <nageshkumarbs007@gmail.com>
2024-02-23 17:46:42 +0530

    Created register page

--- SwagStore/Controllers/RegisterController.cs
+++ SwagStore/Controllers/RegisterController.cs
@@ -1,18 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SwagStore.Data;
+using SwagStore.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace SwagStore.Controllers
 {
     public class RegisterController : Controller
     {
-        [HttpGet]
-        public IActionResult Index()
+        private readonly StoreDbContext dBContext;
+
+        public RegisterController(StoreDbContext dBContext)
         {
-            return View("Register");
+            this.dBContext = dBContext;
         }
 
+
+        [HttpPost]
+        public IActionResult PostRegister(Register register)
+        {
+            try
+            {
+                dBContext.Register.Add(register);
+                dBContext.SaveChanges();
+                return RedirectToAction("Index","Home");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        
         public IActionResult PostRegister()
         {
             return View();
         }
+
     }
 }
